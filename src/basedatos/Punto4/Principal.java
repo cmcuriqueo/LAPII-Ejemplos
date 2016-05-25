3058be6b4cd6ed1ff5aec15d43f827e75716774c
cesar <matias_>
2016-05-25 17:47:55 -0300

    Cosas

--- src/basedatos/Punto4/Principal.java
+++ src/basedatos/Punto4/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 
 	public static void main(String[] args) throws SQLException{
 		try {
-			Connection con= Conexion.getConexion("ventas", "postgres", "carrasco");
+			Connection con= Conexion.getConexion("ventas", "postgres", "postgres");
 			Conexion.imprimirMetadataConexion(con);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
